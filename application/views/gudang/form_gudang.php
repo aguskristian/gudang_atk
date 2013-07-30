77e4c85d9195ea04987b1c7578cbef425f21f8af
I Made Agus Kristian <aguscristian21@gmail.com>
2013-07-30 10:27:43 +0800

    crud_gudang

---
+++ application/views/gudang/form_gudang.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+
+<!-- Mirrored from www.portnine.com/contact by HTTrack Website Copier/3.x [XR&CO'2013], Wed, 03 Jul 2013 09:27:00 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<?php $this->load->view('header')?>
+<?php $this->load->view('main_header')?>
+<body>
+<div class="full-page-wrapper">
+<!--  960 Container Start -->
+<div class="container">
+
+	<!-- Contact Form -->
+<div class="eleven columns">
+		<h4 class="headline">Form Input Unit</h4>
+		
+
+		<div class="success-message">
+			<div class="notification success closeable">
+				<p><span>Success!</span> Your message has been sent.</p>
+			</div>
+		</div>
+
+
+		<div id="contact-form">
+            <form accept-charset="UTF-8" class="new_message" id="new_message" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="4/e/8HmCN84QvWqbOVWyVzVjaeK9ql3IzLWgVc6HSSQ=" /></div>
+            
+				<div class="field">
+					<label>Id Gudang</label>
+                    <input id="message_name" name="id_gudang" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Jenis Barang</label>
+                    <input id="message_name" name="jenis_barang" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>No. Inventory</label>
+                    <input id="message_name" name="no_inventory" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Kode Akun</label>
+                    <input id="message_name" name="kode_akun" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>No. Berita Acara</label>
+                    <input id="message_name" name="berita_acara" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Serial Number</label>
+                    <input id="message_name" name="serial_number" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Spesifikasi</label>
+                    <input id="message_name" name="spesifikasi" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Penempatan awal</label>
+                    <input id="message_name" name="awal" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Penempatan akhir</label>
+                    <input id="message_name" name="akhir" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Update By</label>
+                    <input id="message_name" name="update_by" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Update On</label>
+                    <input id="message_name" name="update_on" size="30" type="text" />
+				</div>
+				
+				<div class="field">
+					<label>Remaks</label>
+                    <textarea name="remaks" class="form-control" rows="3"></textarea>
+				</div>
+				
+				
+				<div class="form-group">
+				  <label for="exampleInputFile">Select File</label>
+				  <input type="file" id="exampleInputFile">
+				  <p class="help-block">Example block-level help text here.</p>
+				</div>
+				<div class="form-group">
+				  <label for="exampleInputFile">Select Image</label>
+				  <input type="file" id="exampleInputFile">
+				  <p class="help-block">Example block-level help text here.</p>
+				</div>
+				<div class="field">
+                    <input type="submit" value="Add" name="submit" />
+				</div>
+			</form>		
+		</div>
+</div>
+		<?php $this->load->view('footer')?>
+</body>
+
+<!-- Mirrored from www.portnine.com/contact by HTTrack Website Copier/3.x [XR&CO'2013], Wed, 03 Jul 2013 09:27:00 GMT -->
+</html>
+
+
